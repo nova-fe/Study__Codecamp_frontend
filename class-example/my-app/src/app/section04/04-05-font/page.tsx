2c4ecf4bab598c654db220ac52215dcd6af1aa1c
Nova <heetask@gmail.com>
2025-01-12 22:48:26 +0900

    [04-04] Next 내장 기능 (폰트, 이미지)

---
+++ class-example/my-app/src/app/section04/04-05-font/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import styles from "./styles.module.css"
+
+export default function FontPage() {
+  return (
+    <>
+      <div>글로벌 font 보여주기</div>
+      <div className={styles.폰트를보여주자}>지정한 font 보여주기</div>
+    </>
+  )
+
+};
