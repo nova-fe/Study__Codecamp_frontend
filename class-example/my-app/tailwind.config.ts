4005ea96e1a05ddcbb790e9612556e24e2810550
Nova <heetask@gmail.com>
2025-01-12 22:17:34 +0900

    [04-03] CSS-Module

--- class-example/my-app/tailwind.config.ts
+++ class-example/my-app/tailwind.config.ts
@@ -11,6 +11,9 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        myColor1: "blue",
+        myColor2: "green"
       },
     },
   },
