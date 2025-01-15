d0bdbe0a6cf7250827c20ee0fab20c50b6b60c62
Nova <heetask@gmail.com>
2025-01-15 22:59:48 +0900

    [07-03] jsonplaceholder -> firebase 로 변경

--- class-example/my-app/src/app/section07/07-04-dynamic-routing-board-post-moved/[id]/page.tsx
+++ class-example/my-app/src/app/section07/07-04-dynamic-routing-board-post-moved/[id]/page.tsx
@@ -4,21 +4,22 @@ import { useParams } from 'next/navigation'; // import { useRouter } from 'next/
 import { useEffect, useState } from 'react';
 
 export default function StaticRoutingMovedPage() {
-  // 구버전 방식(useRouter)
-  // const router = useRouter();
-  // router.push("")
-  // router.query.qqq
+  interface IProps {
+    writer: string;
+    title: string;
+    contents: string;
+  }
 
   // 신버전 방식(useParams)
   // URL에서 동적 파라미터 가져오기 (구조분해할당으로 id 가져옴)
   const { id } = useParams();
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<IProps | undefined>();
 
   useEffect(() => {
     const fetchPost = async () => {
       try {
         const response = await axios.get(
-          `https://jsonplaceholder.typicode.com/posts/${id}`,
+          `https://nova-codecamp-board-default-rtdb.firebaseio.com/class-example/${id}.json`,
         );
         setData(response.data);
       } catch (error) {
@@ -33,9 +34,9 @@ export default function StaticRoutingMovedPage() {
   return (
     <div>
       <div>상세페이지 이동이 완료되었습니다.</div>
-      <div>작성자: {data?.userId}</div>
+      <div>작성자: {data?.writer}</div>
       <div>제목: {data?.title}</div>
-      <div>내용: {data?.body}</div>
+      <div>내용: {data?.contents}</div>
     </div>
   );
 }
--- class-example/my-app/src/app/section07/07-04-dynamic-routing-board-post/page.tsx
+++ class-example/my-app/src/app/section07/07-04-dynamic-routing-board-post/page.tsx
@@ -5,53 +5,52 @@ import axios from 'axios';
 import { useRouter } from 'next/navigation';
 
 export default function AxiosPage() {
-  const [userId, setUserId] = useState('');
+  const [writer, setWriter] = useState('');
   const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [contents, setContents] = useState('');
   const router = useRouter();
 
   const data = {
-    userId,
+    writer,
     title,
-    body: content,
+    contents,
   };
 
   const onClickSubmit = async () => {
     try {
+      // Firebase에 게시글 추가
       const response = await axios.post(
-        'https://jsonplaceholder.typicode.com/posts',
+        'https://nova-codecamp-board-default-rtdb.firebaseio.com/class-example.json',
         data,
       );
 
-      const newPost = response.data;
-      // 게시글 작성이 성공하면 상세 페이지로 이동
-      // router.push(
-      //   `/section07/07-04-dynamic-routing-board-post-moved/${newPost.id}`,
-      // );
-
       alert('게시글 등록이 완료되었습니다.');
-      router.push(`/section07/07-04-dynamic-routing-board-post-moved/33`); // Mock API 사용으로 인해 고정 id 임시 사용
+
+      const boardId = response.data.name; // Firebase가 반환하는 자동 생성된 ID
+      router.push(
+        `/section07/07-04-dynamic-routing-board-post-moved/${boardId}`,
+      ); // Mock API 사용으로 인해 고정 id 임시 사용
     } catch (error) {
       console.log(error);
     }
   };
 
-  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) => {
-    setUserId(e.target.value);
+  const onChangeWriter = (e: ChangeEvent<HTMLInputElement>) => {
+    setWriter(e.target.value);
   };
   const onChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
   };
-  const onChangeContent = (e: ChangeEvent<HTMLInputElement>) => {
-    setContent(e.target.value);
+  const onChangeContents = (e: ChangeEvent<HTMLInputElement>) => {
+    setContents(e.target.value);
   };
 
   // 한 줄일 때는 괄호() 필요 없음
   return (
     <>
-      작성자: <input type="text" onChange={onChangeUserId} /> <br />
+      작성자: <input type="text" onChange={onChangeWriter} /> <br />
       제목: <input type="text" onChange={onChangeTitle} /> <br />
-      내용: <input type="text" onChange={onChangeContent} /> <br />
+      내용: <input type="text" onChange={onChangeContents} /> <br />
       <button onClick={onClickSubmit}>게시글 등록하기</button>
     </>
   );
