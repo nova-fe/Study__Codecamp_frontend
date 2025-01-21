9828c5b02fdc50b94ac7b12ee240072418ee3eff
Nova <heetask@gmail.com>
2025-01-21 09:32:27 +0900

    [12-04] 라이브러리 - 우편번호 모달

--- class-example/my-app/src/app/section12/12-07-modal-custom-address/page.tsx
+++ class-example/my-app/src/app/section12/12-07-modal-custom-address/page.tsx
@@ -20,6 +20,7 @@ export default function ModalCustomPage() {
   };
 
   const handleComplete = data => {
+    // data 에 주소 정보들이 들어옴
     console.log(data);
     setIsModalOpen(false);
   };
@@ -27,17 +28,17 @@ export default function ModalCustomPage() {
   return (
     <>
       <button onClick={showModal}>모달창 열기!</button>
-      {/* <Modal
-        open={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        비밀번호 입력: <input type="password" />
-      </Modal> */}
-
-      <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <DaumPostcodeEmbed onComplete={handleComplete} />
+      {/* 모달종료방식 - 1. 모달 숨김(ex, 이력서 등 닫아도 글이 날라가지 않아야 하는 경우) */}
+      <Modal open={true} onOk={handleOk} onCancel={handleCancel}>
+        게시글 입력: <input type="text" />
       </Modal>
+
+      {/* 모달종료방식 - 2. 모달 초기화(삭제) (ex, 신용카드, 비밀번호 등) */}
+      {isModalOpen && ( // isModalOpen, open{true} 를 통한 모달창 초기화
+        <Modal open={true} onOk={handleOk} onCancel={handleCancel}>
+          <DaumPostcodeEmbed onComplete={handleComplete} />
+        </Modal>
+      )}
     </>
   );
 }
