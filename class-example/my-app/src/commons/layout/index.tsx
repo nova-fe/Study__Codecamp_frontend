416c26345ac93bf6eb6837c131001667ce843580
Nova <heetask@gmail.com>
2025-01-24 17:53:38 +0900

    [13-04] 레이아웃 숨기기

--- class-example/my-app/src/commons/layout/index.tsx
+++ class-example/my-app/src/commons/layout/index.tsx
@@ -1,14 +1,34 @@
 'use client';
 
+import { usePathname } from 'next/navigation';
 import LayoutBanner from './banner';
 import LayoutFooter from './footer';
 import LayoutHeader from './header';
 import LayoutNavigation from './navigation';
 
-export default function Layout({ children }) {
+// 헤더 숨기고 싶은 페이지
+const HIDDEN_HEADERS = [
+  '/section12/12-01-library-icon',
+  '/section12/12-03-library-star',
+  // ...
+];
+
+interface ILayout {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: ILayout) {
+  const pathName = usePathname();
+
+  console.log('===========================');
+  console.log('pathName: ' + pathName);
+  console.log('===========================');
+
+  const isHiddenHeader = HIDDEN_HEADERS.includes(pathName);
+
   return (
     <>
-      <LayoutHeader />
+      {!isHiddenHeader && <LayoutHeader />}
       <LayoutBanner />
       <LayoutNavigation />
       <div className="h-30 flex">
